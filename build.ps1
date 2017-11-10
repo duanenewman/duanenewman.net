fd89f618c82a32abd5dcc37b3248efbd08fed5c0
Duane Newman <duane@alienarc.com>
2017-11-09 20:12:20 -0600

    added a build script

---
+++ build.ps1
@@ -0,0 +1,20 @@
+param (
+	[parameter(Position=0,
+	   Mandatory=$false)]
+	   [ValidateSet("Debug","Deploy")]
+	   [string]$Action
+)
+
+if ($Action -eq ""){
+	echo "No action specified, begining debug session"
+	$Action = "Debug" 
+}
+
+if($Action -eq "Debug") {
+	hugo server -w -D
+} else {
+	echo "deploying"
+	rm -ErrorAction SilentlyContinue -r -force ../duanenewman.github.io/*
+	hugo -d ../duanenewman.github.io/
+	cp file.cname ../duanenewman.github.io/CNAME
+}
